fe70235841dc06837c3243bdd170ef63e42c9a11
Cosmos <bgajdkos@gmail.com>
2024-07-13 19:54:00 +0200

    init

---
+++ src/components/EntryEditor.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import EmojiField from "@/components/EmojiField.vue";
+import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import { computed, ref } from 'vue';
+import type { Emoji } from "@/types/Emoji";
+const text = ref('');
+const emoji = ref<Emoji | null>(null);
+const charCount = computed(() => text.value.length);
+</script>
+<template>
+  <form class="entry-form" @submit.prevent>
+    <textarea v-model="text" placeholder="New Journal Entry for danielkelly_io"></textarea>
+    <EmojiField v-model="emoji" />
+    <div class="entry-form-footer">
+      <span>{{ charCount }} / 280</span>
+      <button>Remember
+        <ArrowCircleRight width="20" />
+      </button>
+    </div>
+  </form>
+</template>
---
+++ src/types/Emoji.ts
@@ -0,0 +1,12 @@
+export type Emoji =
+  | 'confused'
+  | 'cool'
+  | 'crying'
+  | 'happy'
+  | 'heart-eyes'
+  | 'laugh'
+  | 'sad'
+  | 'shocked'
+  | 'smiling'
+  | 'wink'
+  | 'worried'
